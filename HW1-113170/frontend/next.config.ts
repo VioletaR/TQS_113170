ab1cff40aea91ba784a7518303cc82487187a8be
VioletaR <violetabatistaramos@gmail.com>
2025-04-09 22:41:07 +0100

    done

--- HW1-113170/frontend/next.config.ts
+++ HW1-113170/frontend/next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig:NextConfig = {
     allowedDevOrigins: ['localhost', '*.localhost', '0.0.0.0'],
-    
-
+    output: "standalone"
 }
 
 
